3f6bcc66a7aa364c772b530b3a838ef945b27210
mgadelha <mauricio_gadelha@hotmail.com>
2020-07-19 13:08:31 -0700

    Init commit

--- plot_sv.py
+++ plot_sv.py
@@ -2,6 +2,7 @@
 
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Read and parse CSV data
 def readcsv(filename):
@@ -17,6 +18,7 @@ def readcsv(filename):
     values = ['IA', 'IB', 'IC', 'IN', 'VA', 'VB', 'VC', 'VN']
     parsedata['value'] = dict((k, []) for k in values)
 
+    # Parse Wireshark CSV collumn data
     for item in data[1:]:
 
         parsedata['No.'].append(int(item[0]))
@@ -28,32 +30,44 @@ def readcsv(filename):
         parsedata['Time delta from previous displayed frame'].append(
             float(item[6]))
         parsedata['smpCnt'].append(int(item[7]))
-        for key, val in zip(values, item[8:16]):
+        if item[8] == 'global':
+            smpsynch = 2
+        elif item[8] == 'local':
+            smpsynch = 1
+        else:
+            smpsynch = 0
+        parsedata['smpSynch'].append(int(smpsynch))
+        for key, val in zip(values, item[9:17]):
             parsedata['value'][key].append(float(val))
-        parsedata['Info'].append(item[9])
-
+        
     return parsedata
 
 def plot(dictdata):
     """Plot SV data"""
-    fig1, ax1 = plt.subplots()
+    fig, ax = plt.subplots(3,1, constrained_layout=True)
 
     plt.figure(1)
-    ax1.set_ylabel('smpCnt')
-    ax1.set_xlabel('Packet No.')
-
-    ax1.plot(dictdata['Time'], dictdata['value']['IA'], '-', color='blue',
-             label='IA (A Primary)')
-
-    plt.title(
-        'Number Packets x Time delta from previous captured frame',
-        fontsize=18,
-        fontweight='bold')
+    plt.suptitle('Merging Unit', fontsize=18, fontweight='bold')
     plt.legend(loc='upper right', fontsize=14)
-    plt.grid(True)
+
+    ax[0].set_ylabel('IA')
+    ax[0].set_xlabel('Time')
+    ax[0].set_title('IA (A Primary)', fontsize=12)
+    ax[0].plot(dictdata['Time'], np.multiply(dictdata['value']['IA'],0.001), '-*', color='blue')
+           
+    ax[1].set_ylabel('SmpCnt')
+    ax[1].set_xlabel('Time')
+    ax[1].set_title('smpCnt',  fontsize=12)
+    ax[1].plot(dictdata['Time'], dictdata['smpCnt'], '-', color='red')
+
+    ax[2].set_ylabel('smpSynch')
+    ax[2].set_xlabel('Time')
+    ax[2].set_title('smpSynch',  fontsize=12)
+    ax[2].plot(dictdata['Time'], dictdata['smpSynch'], '-', color='red')           
+             
     plt.show()
 
 if __name__ == '__main__':
     print('Normal_traffic_NIC_COFFEE.csv')
-    DATA = readcsv('Normal_traffic_NIC_COFFEE.csv')
+    DATA = readcsv('SV_normal.csv')
     plot(DATA)
